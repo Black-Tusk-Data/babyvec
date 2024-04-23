ff5ae7175e0763db3b869eb070b99c5dff469db0
Liam <liamt@ensemble.com>
2024-04-23 14:56:25 -0700

    Adjust interface

--- babyvec/__init__.py
+++ babyvec/__init__.py
@@ -1,25 +1,25 @@
 from babyvec.computer.embedding_computer_jina_bert import EmbeddingComputerJinaBert
+from babyvec.computer.parallelized_embedding_computer import ParallelizedEmbeddingComputer
 from babyvec.embed_provider.cached_embed_provider import CachedEmbedProvider
+from babyvec.embed_provider.parallelized_cached_embed_provider import ParallelizedCachedEmbedProvider
 from babyvec.models import *
 from babyvec.store.embedding_store_numpy import EmbeddingStoreNumpy
 
 
 # 'packaged' providers
-class CachedParallelJinaEmbedder(CachedEmbedProvider):
+class CachedParallelJinaEmbedder(ParallelizedCachedEmbedProvider):
     def __init__(
             self,
             persist_dir: str,
             n_computers: int,
             device: str,
     ):
-        computer = EmbeddingComputerJinaBert(
-            compute_options=EmbedComputeOptions(
-                device=device,
-                n_computers=n_computers,
-            )
-        )
         store = EmbeddingStoreNumpy(persist_dir=persist_dir)
         super().__init__(
-            computer=computer,
-            store=store
+            n_computers=n_computers,
+            compute_opttions=EmbedComputeOptions(
+                device=device,
+            ),
+            computer_type=EmbeddingComputerJinaBert,
+            store=store,
         )
---
+++ babyvec/embed_provider/parallelized_cached_embed_provider.py
@@ -0,0 +1,37 @@
+import abc
+from typing import Type
+from babyvec.computer.abstract_embedding_computer import AbstractEmbeddingComputer
+from babyvec.computer.parallelized_embedding_computer import ParallelizedEmbeddingComputer
+from babyvec.embed_provider.abstract_embed_provider import AbstractEmbedProvider
+from babyvec.embed_provider.cached_embed_provider import CachedEmbedProvider
+from babyvec.models import EmbedComputeOptions
+from babyvec.store.abstract_embedding_store import AbstractEmbeddingStore
+
+
+class ParallelizedCachedEmbedProvider(CachedEmbedProvider):
+    def __init__(
+            self,
+            *,
+            n_computers: int,
+            compute_options: EmbedComputeOptions,
+            computer_type: Type[AbstractEmbeddingComputer],
+            store: AbstractEmbeddingStore,
+    ):
+        self.n_computers = n_computers
+        self.compute_options = compute_options
+        self.computer_type = computer_type
+        computer = ParallelizedEmbeddingComputer(
+            n_computers=n_computers,
+            compute_options=compute_options,
+            computer_type=computer_type,
+        )
+
+        super().__init__(computer=computer, store=store)
+        return
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.computer.shutdown()
+        return
