e5e00afa86c205cd012bdd4d68113d7ff23f7292
Liam <liamt@ensemble.com>
2024-06-10 13:56:22 -0700

    version 0.2.0

--- setup.py
+++ setup.py
@@ -13,23 +13,24 @@ requirements = [
     "transformers",
 ]
 
-setup(name='babyvec',
-      version='v0.1.0',
-      description='Natural language embedding tools',
-      author='Liam Tengelis',
-      author_email='liam@blacktuskdata.com',
-      url='https://github.com/lummm/babyvec',
-      packages=find_packages(),
-      package_data={
-          '': ['*.yaml'],
-          "babyvec": ["py.typed"],
-      },
-      install_requires=requirements,
-      extras_require = {
-          "http": [
-              "uvicorn",
-              "fastapi",
-              "pydantic",
-          ],
-      },
+setup(
+    name="babyvec",
+    version="v0.2.0",
+    description="Natural language embedding tools",
+    author="Liam Tengelis",
+    author_email="liam@blacktuskdata.com",
+    url="https://github.com/lummm/babyvec",
+    packages=find_packages(),
+    package_data={
+        "": ["*.yaml"],
+        "babyvec": ["py.typed"],
+    },
+    install_requires=requirements,
+    extras_require={
+        "http": [
+            "uvicorn",
+            "fastapi",
+            "pydantic",
+        ],
+    },
 )
